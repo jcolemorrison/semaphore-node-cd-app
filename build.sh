b1169c70ed1be21bebfc8afef94cc62d96997fa9
J Cole Morrison <j.cole.morrison@gmail.com>
2017-03-12 20:52:39 -0700

    add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Building image..."
+docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$REVISION .
+echo "Pushing image"
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$REVISION
+echo "Updating CFN"
+aws cloudformation update-stack --stack-name $STACK_NAME --use-previous-template --capabilities CAPABILITY_IAM \
+  --parameters ParameterKey=DockerImageURL,ParameterValue=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$REVISION \
+  ParameterKey=DesiredCapacity,UsePreviousValue=true \
+  ParameterKey=InstanceType,UsePreviousValue=true \
+  ParameterKey=KeyPairName,UsePreviousValue=true \
+  ParameterKey=MaxSize,UsePreviousValue=true \
+  ParameterKey=SubnetIDs,UsePreviousValue=true \
+  ParameterKey=VpcId,UsePreviousValue=true
